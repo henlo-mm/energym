36426c6df20bd99e23d10ac0e8044d7077dae0ff
henlo-mm <esperanza.olivo@correounivalle.edu.co>
2022-06-04 19:17:12 -0500

    connected express (backend) with react (frontend)

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Holi!" });
+  });
+  
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
