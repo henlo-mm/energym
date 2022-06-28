1859730e07ee8d2f4efec53e31231d572bb715a1
EsperanzaGB <esperanzagbmediagroup@gmail.com>
2022-06-28 16:20:18 -0500

    database connected

---
+++ server/controllers/users.controller.js
@@ -0,0 +1,158 @@
+const db = require("../models");
+const User = db.users;
+const Op = db.Sequelize.Op;
+
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.first_name && !req.body.last_name && !req.body.email && !req.body.password) {
+      res.status(400).send({
+        message: "Content can not be empty!"
+      });
+      return;
+    }
+  
+   
+    const users = {
+        first_name: req.body.first_name,
+        middle_name: req.body.middle_name,
+        last_name: req.body.last_name,
+        second_last_name: req.body.second_last_name,
+        document_type: req.body.document_type,
+        document_number: req.body.document_number,
+        birth_date: req.body.birth_date,
+        email: req.body.email,
+        password: req.body.password,
+        has_membership: req.body.has_membership ? req.body.has_membership : false
+    };
+
+
+    User.create(users)
+    .then(data => {
+        res.send(data);
+       
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the Tutorial."
+        });
+      });
+  };
+  
+ 
+  exports.findAll = (req, res) => {
+    const name = req.query.first_name;
+    var condition = name ? { name: { [Op.iLike]: `%${name}%` } } : null;
+  
+    User.findAll({ where: condition })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving Users."
+        });
+      });
+  };
+  
+  // Find a single User with an id
+  exports.findOne = (req, res) => {
+    const id = req.params.id;
+  
+    User.findByPk(id)
+      .then(data => {
+        if (data) {
+          res.send(data);
+        } else {
+          res.status(404).send({
+            message: `Cannot find User with id=${id}.`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error retrieving User with id=" + id
+        });
+      });
+  };
+  
+  // Update a User by the id in the request
+  exports.update = (req, res) => {
+    const id = req.params.id;
+  
+    User.update(req.body, {
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "User was updated successfully."
+          });
+        } else {
+          res.send({
+            message: `Cannot update User with id=${id}. Maybe User was not found or req.body is empty!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error updating User with id=" + id
+        });
+      });
+  };
+  
+  // Delete a User with the specified id in the request
+  exports.delete = (req, res) => {
+    const id = req.params.id;
+  
+    User.destroy({
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "User was deleted successfully!"
+          });
+        } else {
+          res.send({
+            message: `Cannot delete User with id=${id}. Maybe User was not found!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Could not delete User with id=" + id
+        });
+      });
+  };
+  
+  // Delete all Users from the database.
+  exports.deleteAll = (req, res) => {
+    User.destroy({
+      where: {},
+      truncate: false
+    })
+      .then(nums => {
+        res.send({ message: `${nums} Users were deleted successfully!` });
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while removing all Users."
+        });
+      });
+  };
+  
+  exports.findAllPublished = (req, res) => {
+    User.findAll({ where: { has_membership: true } })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving Users."
+        });
+      });
+  };
--- server/server.js
+++ server/server.js
@@ -2,13 +2,35 @@ const express = require("express");
 
 const PORT = process.env.PORT || 5000;
 
+const cors = require("cors");
+
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Holi!" });
-  });
-  
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./models");
+
+//db.sequelize.sync();
+
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+ });
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+require("./routes/user.routes")(app);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+  
