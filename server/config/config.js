d71df6638880c1e0bb21f214616d48522888bb5d
EsperanzaGB <esperanzagbmediagroup@gmail.com>
2022-08-10 15:46:43 -0500

    update db

--- server/config/config.js
+++ server/config/config.js
@@ -16,10 +16,16 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
+    "username": "ihkdysdwgqlwki",
+    "password": "48fa146fe1b1b95f1104cd432bcce6ce9d12cadf9c66d1516386022db6b6930a",
     "database": "DATABASE_URL",
-    "host": "energym-point-prod.herokuapp.com",
-    "dialect": "postgres"
+    "host": "ec2-44-206-137-96.compute-1.amazonaws.com",
+    "dialect": "postgres",
+    "dialectOptions": {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
   }
 }
