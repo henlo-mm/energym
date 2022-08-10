54b48680e44182ed6f427ca8070e87fa3f4e772b
henlo-mm <esperanza.olivo@correounivalle.edu.co>
2022-08-10 18:49:31 -0500

    update deploy

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -70,7 +70,7 @@ exports.signUp = async (req, res) => {
         if (!password_valid) {
           res.status(400).json({ error : "Invalid Password" });
         }
-        const token = jwt.sign({ id: user.id }, config.secret, {
+        const token = jwt.sign({ id: user.id }, process.env.secret, {
           expiresIn: 1800, // 30 min
         });
     
